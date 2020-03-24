2e2e5b200b8f1830fcc50a923ebfb01d952c4323
Tasha Stenner <Tasha.Stenner@Nology.local>
2020-03-24 12:49:46 +0000

    added-instructions

--- src/Containers/InputFace/InputFace.jsx
+++ src/Containers/InputFace/InputFace.jsx
@@ -5,15 +5,16 @@ import InputField from "../../Components/InputField";
 
 const InputFace = () => {
   const [currentAngle, updateAngle] = useState(0);
- 
 
   return (
-  
     <section>
       <Face angle={currentAngle} />
       <InputField handleInput={value => updateAngle(value)} />
-      </section>
- 
+      <h3>
+        Input a angle into the box above, click on Shea's face to transform his
+        eyes to that angle. Click on his face to reset
+      </h3>
+    </section>
   );
 };
 
